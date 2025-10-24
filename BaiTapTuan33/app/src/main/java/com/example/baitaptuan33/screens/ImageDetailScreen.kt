4a9fbcae647181bbabc2359dee62aec1e574bf6c
HoangPC2801 <hoangtucuncon5s1@gmail.com>
2025-10-24 18:07:18 +0700

    Thêm thư mục BaiTapTuan3

---
+++ BaiTapTuan33/app/src/main/java/com/example/baitaptuan33/screens/ImageDetailScreen.kt
@@ -0,0 +1,49 @@
+package com.example.baitaptuan33.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.baitaptuan33.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ImageDetailScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Image Detail") },
+                navigationIcon = {
+                    // Nút để quay lại màn hình trước
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        // Box để căn giữa hình ảnh
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.man),
+                contentDescription = "Sample Image",
+                modifier = Modifier.size(250.dp)
+            )
+        }
+    }
+}
