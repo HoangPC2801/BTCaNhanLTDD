de85f5bd459fa905dea8dec6311ba14859a800aa
HoangPC2801 <hoangtucuncon5s1@gmail.com>
2025-11-06 23:19:26 +0700

    Them thu muc Demo Navigation UI

---
+++ DogChat/app/src/main/java/com/hfad/dogchat/InboxFragment.kt
@@ -0,0 +1,27 @@
+package com.hfad.dogchat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hfad.dogchat.databinding.FragmentInboxBinding
+
+class InboxFragment : Fragment() {
+
+    private var _binding: FragmentInboxBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentInboxBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
